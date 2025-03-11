75155184b89c0f9ce30c236b447b4c772475782b
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2025-03-11 23:19:40 +0900

    ✨ feat: add TodoList component and integrate AWS Amplify for todo management

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 
 const app = createApp(App)
-
+Amplify.configure(outputs)
 app.use(router)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <TodoList />
   </div>
 </template>
+
+<script setup lang="ts">
+import TodoList from '@/components/TodoList.vue'
+</script>
 
 <style>
 @media (min-width: 1024px) {
